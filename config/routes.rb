8d46dd90e29c6be4688c1454520a7bc925e9723e
summeriver13 <summeriver13@163.com>
2024-04-02 15:32:01 +0800

    add upload routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "/course", to: "courses#index"
   get "/classroom", to: "classrooms#index"
   get "/about", to: "about#index"
+  get "/upload", to: "uploads#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
