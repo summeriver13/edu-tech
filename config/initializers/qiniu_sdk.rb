63de8fc259506fd0cb7ef6593e6053b5b23d0a72
summeriver13 <summeriver13@163.com>
2024-04-09 09:53:17 +0800

    qiniu

--- config/initializers/qiniu_sdk.rb
+++ config/initializers/qiniu_sdk.rb
@@ -1,6 +1,6 @@
 require 'qiniu'
 
-Qiniu.establish_connection! access_key: '0UHnMYqWItMk_9iDZIkTcjW_YcMR7UU5prT7rBZt',
-                            secret_key: 'o_kS6RVesdMowl2eJam49OQcLg4YYFMjyL_mudWO'
+Qiniu.establish_connection! access_key: '',
+                            secret_key: ''
 
-Rails.application.config.qiniu_domain = "qiniu.summeriver.tech"
+Rails.application.config.qiniu_domain = ""
