0a6dffb7a3a5eadf549b4d29afa7c5e4a8f08470
summeriver13 <summeriver13@163.com>
2024-04-09 09:54:57 +0800

    route optimization

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
 Rails.application.routes.draw do
-  resources :courses
- root "home#index"
+  root "home#index"
 
-  get "/home", to: "home#index"
-  get "/course", to: "courses#index"
-  get "/classroom", to: "classrooms#index"
-  get "/about", to: "about#index"
-  get "/upload", to: "uploads#index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :home , only: [:index]
+  resources :courses do
+    member do
+      get 'information'
+    end
+    resources :resources
+    resources :activities
+  end
+  resources :users
 end
